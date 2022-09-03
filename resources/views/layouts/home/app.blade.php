b5fffd2d84abe76821ef7df219132d15405ae174
shinnichiro <shinnichiro.mm@icloud.com>
2022-09-03 17:24:56 +0900

    edit README.md

--- resources/views/layouts/home/app.blade.php
+++ resources/views/layouts/home/app.blade.php
@@ -8,12 +8,12 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
         <!--load all Font Awesome styles -->
-        <link href="./fontawesome/css/all.css" rel="stylesheet">
+        <link href="{{ asset('/fontawesome/css/all.css') }}" rel="stylesheet">
 
         <!-- Option 1: Bootstrap Bundle with Popper -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-        <link rel="stylesheet" href="./css/index.css">
+        <link rel="stylesheet" href="{{ asset('/css/index.css') }}">
         <script src="js/"></script>
 
         <title>XX塾</title>
--- routes/web.php
+++ routes/web.php
@@ -26,34 +26,36 @@ Route::get('inquiry', [InquiriesController::class, 'index'])->name('inquiry.inde
 Route::post('inquiry', [InquiriesController::class, 'create'])->name('inquiry.create');
 Auth::routes();
 
-Route::get('user/index', [UsersController::class, 'index'])->name('user.index');
-Route::get('user/topics/show', [TopicsController::class, 'show'])->name('user.topics.show');
-Route::get('user/topics/{id}/edit', [TopicsController::class, 'edit'])->name('user.topics.edit');
-Route::post('user/topics/{id}/store', [TopicsController::class, 'store'])->name('user.topics.store');
-Route::post('user/topics/create', [TopicsController::class, 'create'])->name('user.topics.create');
-Route::delete('user/topics/{id}/destroy', [TopicsController::class, 'destroy'])->name('user.topics.destroy');
+Route::group(['middleware' => ['auth']], function() {
+    Route::get('user/index', [UsersController::class, 'index'])->name('user.index');
+    Route::get('user/topics/show', [TopicsController::class, 'show'])->name('user.topics.show');
+    Route::get('user/topics/{id}/edit', [TopicsController::class, 'edit'])->name('user.topics.edit');
+    Route::post('user/topics/{id}/store', [TopicsController::class, 'store'])->name('user.topics.store');
+    Route::post('user/topics/create', [TopicsController::class, 'create'])->name('user.topics.create');
+    Route::delete('user/topics/{id}/destroy', [TopicsController::class, 'destroy'])->name('user.topics.destroy');
 
-Route::get('user/inquiry/show', [InquiriesController::class, 'show'])->name('user.inquiry.show');
-Route::post('user/inquiry/{id}/store', [InquiriesController::class, 'store'])->name('user.inquiry.store');
+    Route::get('user/inquiry/show', [InquiriesController::class, 'show'])->name('user.inquiry.show');
+    Route::post('user/inquiry/{id}/store', [InquiriesController::class, 'store'])->name('user.inquiry.store');
 
-Route::get('user/score/index', [ScoresController::class, 'index'])->name('user.score.index');
-Route::get('user/score/show', [ScoresController::class, 'show'])->name('user.score.show');
-Route::post('user/score/show', [ScoresController::class, 'show'])->name('user.score.show');
-Route::post('user/score/create', [ScoresController::class, 'create'])->name('user.score.create');
-Route::post('user/score/{id}/edit', [ScoresController::class, 'edit'])->name('user.score.edit');
-Route::get('user/score/{id}/edit', [ScoresController::class, 'edit'])->name('user.score.edit');
-Route::post('user/score/{id}/store', [ScoresController::class, 'store'])->name('user.score.store');
-Route::delete('user/score/{id}/destroy', [ScoresController::class, 'destroy'])->name('user.score.destroy');
+    Route::get('user/score/index', [ScoresController::class, 'index'])->name('user.score.index');
+    Route::get('user/score/show', [ScoresController::class, 'show'])->name('user.score.show');
+    Route::post('user/score/show', [ScoresController::class, 'show'])->name('user.score.show');
+    Route::post('user/score/create', [ScoresController::class, 'create'])->name('user.score.create');
+    Route::post('user/score/{id}/edit', [ScoresController::class, 'edit'])->name('user.score.edit');
+    Route::get('user/score/{id}/edit', [ScoresController::class, 'edit'])->name('user.score.edit');
+    Route::post('user/score/{id}/store', [ScoresController::class, 'store'])->name('user.score.store');
+    Route::delete('user/score/{id}/destroy', [ScoresController::class, 'destroy'])->name('user.score.destroy');
 
-Route::get('user/message/index', [MessagesController::class, 'index'])->name('user.message.index');
-Route::post('user/message/create', [MessagesController::class, 'create'])->name('user.message.create');
-Route::get('user/message/{id}/show', [MessagesController::class, 'show'])->name('user.message.show');
-Route::post('user/message/{id}/store', [MessagesController::class, 'store'])->name('user.message.store');
-Route::delete('user/message/{id}/destroy', [MessagesController::class, 'destroy'])->name('user.message.destroy');
+    Route::get('user/message/index', [MessagesController::class, 'index'])->name('user.message.index');
+    Route::post('user/message/create', [MessagesController::class, 'create'])->name('user.message.create');
+    Route::get('user/message/{id}/show', [MessagesController::class, 'show'])->name('user.message.show');
+    Route::post('user/message/{id}/store', [MessagesController::class, 'store'])->name('user.message.store');
+    Route::delete('user/message/{id}/destroy', [MessagesController::class, 'destroy'])->name('user.message.destroy');
 
-Route::get('user/auth', [UsersController::class, 'auth'])->name('user.auth');
-Route::post('user/auth/show', [UsersController::class, 'show'])->name('user.auth.show');
-Route::get('user/auth/show', [UsersController::class, 'show'])->name('user.auth.show');
-Route::post('user/auth/{id}/store', [UsersController::class, 'store'])->name('user.auth.store');
-Route::get('user/auth/{id}/store', [UsersController::class, 'store'])->name('user.auth.store');
-Route::delete('user/auth/{id}/destroy', [UsersController::class, 'destroy'])->name('user.auth.destroy');
+    Route::get('user/auth', [UsersController::class, 'auth'])->name('user.auth');
+    Route::post('user/auth/show', [UsersController::class, 'show'])->name('user.auth.show');
+    Route::get('user/auth/show', [UsersController::class, 'show'])->name('user.auth.show');
+    Route::post('user/auth/{id}/store', [UsersController::class, 'store'])->name('user.auth.store');
+    Route::get('user/auth/{id}/store', [UsersController::class, 'store'])->name('user.auth.store');
+    Route::delete('user/auth/{id}/destroy', [UsersController::class, 'destroy'])->name('user.auth.destroy');
+});
