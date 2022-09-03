f1b6ef24fe01249462e73ca7ca4e1a1232b29d27
shinnichiro <shinnichiro.mm@icloud.com>
2022-09-03 17:49:52 +0900

    edit http to https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         \Schema::defaultStringLength(191);
-        \URL::forceScheme('http');
+        \URL::forceScheme('https');
     }
 }
